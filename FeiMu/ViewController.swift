22296497ed46005d649d1550475a559d207eaf95
金悦悦 <921386013@qq.com>
2018-11-02 17:48:34 +0800

    no message

--- FeiMu/ViewController.swift
+++ FeiMu/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import SBCycleScrollView
+import IQKeyboardManagerSwift
+import ESPullToRefresh
 
 class ViewController: UIViewController {
+    
+    private lazy var tableView : UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        IQKeyboardManager.shared.enable = true
+        
+        //测试轮播图
+        let cycleScrollView = CycleScrollView.initScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 200), imageNamesGroup: ["Image1","Image2","Image3"], cycleOptions: CycleOptions())
+        view.addSubview(cycleScrollView)
+        
+        let cycleScrollView1 = CycleScrollView.initScrollView(frame: CGRect(x: 0, y: 220, width: view.frame.width, height: 200), delegate: self, placehoder: UIImage(named: "Image1"), cycleOptions: CycleOptions())
+        view.addSubview(cycleScrollView1)
+        
+        cycleScrollView1.imageURLStringsGroup = ["http://pic33.photophoto.cn/20141022/0019032438899352_b.jpg","http://img.zcool.cn/community/010a1b554c01d1000001bf72a68b37.jpg@1280w_1l_2o_100sh.png","http://img.zcool.cn/community/012b245544ebd60000019ae93e4e4b.jpg@1280w_1l_2o_100sh.jpg"]
+        
+        let textField = UITextField(frame: CGRect(x: 0, y: view.frame.maxY - 100, width: view.frame.width, height: 50))
+        textField.backgroundColor = UIColor.yellow
+        view.addSubview(textField)
+        
+        view.addSubview(tableView)
+        //下拉刷新
+        tableView.es.addPullToRefresh {
+            [unowned self] in
+            
+            self.tableView.es.stopPullToRefresh()
+            ///// 设置ignoreFooter来处理不需要显示footer的情况
+            self.tableView.es.stopPullToRefresh(ignoreDate: true, ignoreFooter: false)
+            
+        }
+        
+        //上拉加载更多
+        tableView.es.addInfiniteScrolling {
+            //如果你的加载更多事件成功，调用es_stopLoadingMore()重置footer状态
+            self.tableView.es.stopLoadingMore()
+            //通过noticeNoMoreData设置footer暂无数据状态
+            self.tableView.es.noticeNoMoreData()
+            
+        }
+    
     }
+}
 
-
+extension ViewController :  CycleScrollViewDelegate {
+    
+    func didSelectedCycleScrollView(_ cycleScrollView: CycleScrollView, _ Index: NSInteger) {
+        print("点击了第\(Index)个")
+    }
+    
 }
 
